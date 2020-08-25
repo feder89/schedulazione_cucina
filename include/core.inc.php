fcba6e05668f49c660ee64d291c648516af55b06
federico.quaglia <federico.quaglia@external.thalesgroup.com>
2020-08-25 22:10:21 +0200

    set db name to sett2020

--- include/core.inc.php
+++ include/core.inc.php
@@ -1,6 +1,6 @@
 <?php
 function connectToDb(){
-	$link = mysqli_connect("127.0.0.1", "root", "furente", "gestionale_sett2019");
+	$link = mysqli_connect("127.0.0.1", "root", "furente", "gestionale_sett2020");
 
 
 	if (!$link) {

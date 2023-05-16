42fdaf595524e22f1b98c9d73215255e0217256b
Federico <quagliafederico@gmail.com>
2023-05-16 15:57:28 +0200

    ""

--- include/core.inc.php
+++ include/core.inc.php
@@ -1,6 +1,6 @@
 <?php
 function connectToDb(){
-	$link = mysqli_connect("127.0.0.1", "root", "", "gestionale_sett2022");
+	$link = mysqli_connect("127.0.0.1", "root", "", "gestionale_giu2023");
 
 
 	if (!$link) {

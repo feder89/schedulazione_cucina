9020b78dbf27059a8f244355f8b15784c7131def
Federico <quagliafederico@gmail.com>
2023-08-28 14:27:36 +0200

    settembre_2023

--- include/core.inc.php
+++ include/core.inc.php
@@ -1,6 +1,6 @@
 <?php
 function connectToDb(){
-	$link = mysqli_connect("127.0.0.1", "root", "furente", "gestionale_giu2023");
+	$link = mysqli_connect("127.0.0.1", "root", "furente", "gestionale_sett2023");
 
 
 	if (!$link) {

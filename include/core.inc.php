cd34cef4abea46164d58d4148de15470dcfa3626
feder89 <quagliafederico@gmail.com>
2024-08-21 16:31:40 +0200

    "db_settembre2024"

--- include/core.inc.php
+++ include/core.inc.php
@@ -1,6 +1,6 @@
 <?php
 function connectToDb(){
-	$link = mysqli_connect("127.0.0.1", "root", "furente", "gestionale_giu2024");
+	$link = mysqli_connect("127.0.0.1", "root", "furente", "gestionale_sett2024");
 
 
 	if (!$link) {
